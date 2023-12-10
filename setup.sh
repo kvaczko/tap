6d4dea61b831f97b3e0583855918796e24aafa3c
kvaczko <43028782+kvaczko@users.noreply.github.com>
2023-12-10 12:36:15 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,16 +2,19 @@
 
 apt -y install ffmpeg pulseaudio alsa-utils pip python3.11-venv sudo zip curl
 
-curl https://github.com/kvaczko/tap/raw/main/tap.zip -o /tap.zip
+curl https://raw.githubusercontent.com/kvaczko/tap/main/tap.zip -o /tap.zip
 cd /
 unzip tap.zip
 
 chmod +x /data/script/*
 
 cp /data/config/sshd_config /etc/ssh/
+service sshd reload
 
 cp /data/config/website.service /etc/systemd/system/
 systemctl daemon-reload
-systemctl enable website.service 
+systemctl enable website.service
+
 crontab </data/config/crontab.txt
+
 reboot
