ff8377780032390e83ede67b03e7614ef212bc49
kvaczko <43028782+kvaczko@users.noreply.github.com>
2023-12-10 12:11:10 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,9 +6,12 @@ curl https://github.com/kvaczko/tap/raw/main/tap.zip -o /tap.zip
 cd /
 unzip tap.zip
 
+chmod +x /data/script/*
+
 cp /data/config/sshd_config /etc/ssh/
 
 cp /data/config/website.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable website.service 
 crontab </data/config/crontab.txt
+reboot
